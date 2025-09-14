ef278b5239d55d17ad9cd5e237706a7d58718f57
Cecil Git <cecilbaraik19@gmail.com>
2025-09-14 16:50:34 +0530

    Details file committed

---
+++ src/Details.java
@@ -0,0 +1,21 @@
+public class Details {
+    public static void main(String[] args)
+    {
+        char grade;
+        grade='A';
+        int roll;
+        roll=104;
+        float per;
+        per=48.456f;
+        double fee;
+        fee=786579;
+        String name;
+        name="Cecil";
+        System.out.println("Name             :"+name);
+        System.out.println("Roll             :"+roll);
+        System.out.println("Fee              :"+fee);
+        System.out.println("Percentage       :"+per);
+        System.out.println("Grade            :"+grade);
+    }
+
+}
