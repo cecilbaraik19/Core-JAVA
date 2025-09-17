1c98247432180e949cf51d4e25ee05b7d3ea000f
Cecil Git <cecilbaraik19@gmail.com>
2025-09-17 22:47:30 +0530

    code for finding result of base and power value

---
+++ src/BasePower.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class BasePower {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int b,p;
+        double r;
+        System.out.println("Enter Base Value & Power Value");
+        b = input.nextInt();
+        p = input.nextInt();
+        r = Math.pow(b,p);
+        System.out.println("Base Value = "+b+" Power Value = "+p+ " & Result = "+r);
+    }
+}
