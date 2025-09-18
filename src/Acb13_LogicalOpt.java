63a6c526ea8a8e28d20ec8b95987097e24a70c27
Cecil Git <cecilbaraik19@gmail.com>
2025-09-19 02:02:38 +0530

    Logical Operator Example

---
+++ src/Acb13_LogicalOpt.java
@@ -0,0 +1,13 @@
+public class Acb13_LogicalOpt {
+    public static void main(String[] args) {
+        boolean a = true, b = false;
+        System.out.println("Value of a     :"+a);
+        System.out.println("Value of b     :"+b);
+        System.out.println("Value of a&&a  :"+(a&&a));
+        System.out.println("Value of a&&b  :"+(a&&b));
+        System.out.println("Value of a||b  :"+(a||b));
+        System.out.println("Value of b||b  :"+(b||b));
+        System.out.println("Value of !a    :"+(!a));
+        System.out.println("Value of !b    :"+(!b));
+    }
+}
