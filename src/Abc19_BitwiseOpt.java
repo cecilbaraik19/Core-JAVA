52d1a0dc4f62a274bb18e1f525d7f7dbd8af9263
Cecil Git <cecilbaraik19@gmail.com>
2025-09-19 02:45:22 +0530

    Bitwise Operator Examples

---
+++ src/Abc19_BitwiseOpt.java
@@ -0,0 +1,12 @@
+public class Abc19_BitwiseOpt {
+    public static void main(String[] args) {
+        int a = 10, b = 15;
+        System.out.println("Value of a    :" + a);
+        System.out.println("Value of b    :" + b);
+        System.out.println("Value of a&b  :" + (a&b));
+        System.out.println("Value of a|b  :" + (a|b));
+        System.out.println("Value of a^b  :" + (a^b));
+        System.out.println("Value of a<<2 :" + (a<<2));
+        System.out.println("Value of a>>a :" + (a>>2));
+    }
+}
