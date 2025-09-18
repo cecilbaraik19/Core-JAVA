f65087293614139943e966bf06cd81e734e2b2df
Cecil Git <cecilbaraik19@gmail.com>
2025-09-19 02:40:32 +0530

    Assignment Opt examples

---
+++ src/Abc18_AssignmentOpt.java
@@ -0,0 +1,8 @@
+public class Abc18_AssignmentOpt {
+    public static void main(String[] args) {
+        int a = 5;
+        System.out.println("Value of a  :"+a);
+        a *= 10;
+        System.out.println("Value of a after assignment operator :"+a);
+    }
+}
