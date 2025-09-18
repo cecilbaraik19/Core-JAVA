ba4d149e20fcbfb53d4d86eea896f29b8f5b42ec
Cecil Git <cecilbaraik19@gmail.com>
2025-09-19 02:37:01 +0530

    Increment and Decrement

---
+++ src/Abc17_Operator.java
@@ -0,0 +1,11 @@
+// Increment and Decrement
+public class Abc17_Operator {
+    public static void main(String[] args) {
+        int a = 5;
+        System.out.println("Value of a in 1st Step   :"+a);
+        System.out.println("Value of a in 2nd Step   :"+(++a));
+        System.out.println("Value of a in 3rd Step   :"+a);
+        System.out.println("Value of a in 4th Step   :"+(a++));
+        System.out.println("Value of a in 5th Step   :"+a);
+    }
+}
