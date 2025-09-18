c0a84ab326990a6130e0d5c538b107b8346fd665
Cecil Git <cecilbaraik19@gmail.com>
2025-09-19 02:18:16 +0530

    Relational Operator Examples

---
+++ src/Abc14_RelationalOpt.java
@@ -0,0 +1,13 @@
+public class Abc14_RelationalOpt {
+        public static void main(String[] args) {
+            int a = 10, b = 15;
+            System.out.println("Value of a     :"+a);
+            System.out.println("Value of b     :"+b);
+            System.out.println("Value of a==a  :"+(a==a));
+            System.out.println("Value of a<=b  :"+(a<=b));
+            System.out.println("Value of a>=b  :"+(a>=b));
+            System.out.println("Value of a<b   :"+(a<b));
+            System.out.println("Value of a>b   :"+(a>b));
+            System.out.println("Value of a!=b  :"+(a!=b));
+        }
+}
