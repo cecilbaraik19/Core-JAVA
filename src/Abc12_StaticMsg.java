bf8a6d0d807cd4c7e85907c2052095e469df7367
Cecil Git <cecilbaraik19@gmail.com>
2025-09-19 01:51:44 +0530

    Static Msg

---
+++ src/Abc12_StaticMsg.java
@@ -0,0 +1,9 @@
+public class Abc12_StaticMsg {
+    public static void main(String[] args) {
+        System.out.println("This Message from main block");
+    }
+    static
+    {
+        System.out.println("This Msg is from static block");
+    }
+}
